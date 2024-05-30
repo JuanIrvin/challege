6f1e969243168defe292e5ee9ec4fc9a7275d632
Juan Elias <juan.elias1194@gmail.com>
2024-05-29 20:49:26 -0600

    Iniciando proyecto pruba de challenge con pnpm

---
+++ backend/server.ts
@@ -0,0 +1,73 @@
+import express from 'express'
+import cors from 'cors'
+import multer from 'multer'
+import csvToJson from 'convert-csv-to-json'
+
+
+const app = express()
+const port = process.env.PORT ?? 3000
+
+const storage = multer.memoryStorage()
+const upload = multer({ storage })
+
+let userData: Array<Record<string, string>> = []
+
+app.use(cors())
+
+app.post('/api/files', upload.single('file'), async (req, res) => {
+
+    const { file } = req
+
+    if (!file) {
+        return res.status(500).json({ message: 'File is required'})
+    }
+
+    if (file.mimetype != 'text/csv') {
+        return res.status(500).json({ message: 'File must be CSV'})
+    }
+
+    let json: Array<Record<string, string>> = []
+
+    try{
+        const rawCsv = Buffer.from(file.buffer).toString('utf-8')
+        console.log(rawCsv)
+
+        json = csvToJson.csvStringToJson(rawCsv)
+    } catch (error) {
+        return res.status(500).json({ message: 'Error parsing the file' })
+    }
+
+    userData = json
+
+    return res.status(200).json({ data: [], message: 'El archivo se cargó correctamente'})
+})
+
+app.get('/api/users', async (req, res) => {
+
+    const {q} = req.query
+
+    if(!q) {
+        return res.status(500).json({
+            message: 'Query param `q` is required'
+        })
+    }
+
+    if (Array.isArray(q)) {
+        return res.status(500).json({
+            message: 'Query param `q` must be a string'
+        })
+    }
+
+    const search = q.toString().toLowerCase()
+
+    const filterData = userData.filter(row => {
+        
+    })
+
+    return res.status(200).json({ data: []})
+})
+
+
+app.listen(port, () => {
+    console.log(`Server is running at http://localhost:${port}`)
+})
